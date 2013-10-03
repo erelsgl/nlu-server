e9c554e0a96e2de948a2d3f18496b0bc5f4552e1
erelsgl <erelsgl@gmail.com>
2013-10-03 14:21:41 +0200

    fix train set

--- train.js
+++ train.js
@@ -29,10 +29,10 @@ var createNewClassifier = function() {
 }
 
 var do_small_test = false;
-var do_small_serialization_test = true;
-var do_cross_dataset_testing = true;
-var do_final_test = true;
-var do_cross_validation = true;
+var do_small_serialization_test = false;
+var do_cross_dataset_testing = false;
+var do_final_test = false;
+var do_cross_validation = false;
 var do_serialization = true;
 var do_test_on_training_data = true;
 
