7a7fc96706cdf8679195f4b7554ce805ac940fe3
vasily <elgesto@gmail.com>
2014-04-05 08:55:27 +0300

    dialogue restoring

--- dialogue_restore/dialogue1.js
+++ dialogue_restore/dialogue1.js
@@ -16,15 +16,18 @@ var trainAndTest_hash= require('limdu/utils/trainAndTest').trainAndTest_hash;
 var partitions = require('limdu/utils/partitions');
 
 
-// jsonfile = "datasets/Employer/nlu_ncagent_students_negonlpnc.json"
-// jsonfile = "datasets/Employer/nlu_ncagent_turkers_negonlpncAMT.json"
-//jsonfile = "../datasets/Employer/5_woz_ncagent_turkers_negonlp2ncAMT.json"
-jsonfile = "../datasets/Employer/nlu_ncagent_turkers_negonlpncAMT.json"
-csvfile = "ncagent.csv"
+  jsonfile = "../datasets/Employer/nlu_ncagent_students_negonlpnc.json"
+//jsonfile = "../datasets/Employer/nlu_ncagent_turkers_negonlpncAMT.json"
+ // jsonfile = "../datasets/Employer/5_woz_ncagent_turkers_negonlp2ncAMT.json"
+//jsonfile = "../datasets/Employer/nlu_ncagent_turkers_negonlpncAMT.json"
+//csvfile = "ncagent.csv"
+csvfile = "overall.csv"
 // 504
 
 jsondataset = JSON.parse(fs.readFileSync(jsonfile))
 csvdataset = fs.readFileSync(csvfile, 'utf8')
+csvdataset = csvdataset.concat(fs.readFileSync("ncagent_formated.csv", 'utf8'))
+
 csvdataset = csvdataset.split('\n')
 
 
@@ -33,9 +36,13 @@ jsoncontent = []
 
 _.each(csvdataset, function(csv, key, list){ 
 	str = csv.split("^");
-	if ((str[9] == 'Message')&&(str[7] == 'Employer'))
+	// console.log(str)
+	// process.exit(0)
+	if ((str[7] == 'Message')&&(str[5] == 'Employer'))
 		{
-			ss = str[5].replace(/\"/g,'');
+			ss = str[3].replace(/\"/g,'');
+			// console.log(ss)
+			// process.exit(0)
 			csvcontent.push(ss)
 		}
 })
@@ -44,13 +51,43 @@ _.each(jsondataset, function(json, key, list){
 	jsoncontent.push(json['input'])
 })
 
+	console.log(csvfile)
+		console.log(jsonfile)
 console.log("csv size "+csvcontent.length)
 console.log("uniq csv size "+(_.uniq(csvcontent)).length)
 console.log("json size "+jsoncontent.length)
 console.log("uniq json size "+(_.uniq(jsoncontent)).length)
 
-diff = _.difference(jsoncontent, csvcontent);
+diff = _.difference(_.uniq(jsoncontent), _.uniq(csvcontent));
 // diff = _.difference(csvcontent, jsoncontent);
 console.log("in json and not in csv")
 console.log(diff.length)
 // _.difference([1, 2, 3, 4, 5], [5, 2, 10]);
+
+// overall.csv
+// ../datasets/Employer/nlu_ncagent_turkers_negonlpncAMT.json
+// csv size 4057
+// uniq csv size 2107
+// json size 336
+// uniq json size 336
+// in json and not in csv
+// 17
+
+// overall.csv
+// ../datasets/Employer/nlu_ncagent_students_negonlpnc.json
+// csv size 4057
+// uniq csv size 2107
+// json size 516
+// uniq json size 516
+// in json and not in csv
+// 236
+
+
+// overall.csv
+// ../datasets/Employer/5_woz_ncagent_turkers_negonlp2ncAMT.json
+// csv size 4057
+// uniq csv size 2107
+// json size 38
+// uniq json size 38
+// in json and not in csv
+// 0
