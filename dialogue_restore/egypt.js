a7851bd90875233a691a5d461cb40b33d0444c53
vasily <elgesto@gmail.com>
2014-08-26 21:43:16 +0300

    dial restore

--- dialogue_restore/egypt.js
+++ dialogue_restore/egypt.js
@@ -2,7 +2,7 @@ var _ = require('underscore')._;
 var fs = require('fs');
 
 // filename = "ncagent.csv"
-filename = "egypt.csv"
+filename = "egypt1.csv"
 
 //jsonfile = "../datasets/Employer/nlu_ncagent_students_negonlpnc.json"
 // jsonfile = "../datasets/Employer/5_woz_ncagent_turkers_negonlp2ncAMT.json"
@@ -25,17 +25,33 @@ _.each(content, function(str, key, list){
 	str = str.split("^");
 	// console.log(str)
 	// process.exit(0)
-	if (str[6]=='Message')
+	if ((str[7]=='Message') && (str[3].length > 2))
 	{
-		if (str[4] == 'Candidate')
+		if (str[5] == 'Candidate')
 			candidate.push(str[3].replace(/\"/g,''))
-		if (str[4] == 'Employer')
+		if (str[5] == 'Employer')
 			employer.push(str[3].replace(/\"/g,''))
 	}
 })
 
+// console.log(JSON.stringify(employer.length, null, 4))
+// console.log(JSON.stringify(candidate.length, null, 4))
+// process.exit(0)
+
+// console.log(JSON.stringify(employer, null, 4))
+// console.log()
+// process.exit(0)
 
 _.each(employer, function(text, key, list){ 
+	console.log(JSON.stringify({"text":text,
+				 "forward":"on",
+				 "explain":"4",
+				 "classifierName":"Employer-egypt",
+				 "timestamp":"2013-12-15T18:16:40.078Z"}))
+
+}, this)
+
+_.each(candidate, function(text, key, list){ 
 	console.log(JSON.stringify({"text":text,
 				 "forward":"on",
 				 "explain":"4",
