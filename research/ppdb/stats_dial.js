f17f68c3b474f192e3b4229efe5eb34a981d6f40
vasily <elgesto@gmail.com>
2014-12-06 17:29:41 +0200

    Add agent counters

--- research/ppdb/stats_dial.js
+++ research/ppdb/stats_dial.js
@@ -10,18 +10,26 @@ data = JSON.parse(fs.readFileSync("../../../datasets/DatasetDraft/dial_usa_rule.
 
 var gooddial = 0
 var newagentgood = 0
+var kbagentgood = 0
 
 _.each(data, function(value, key, list){
 	if (value['status'].indexOf("goodconv") != -1)
 		{
-			gooddial += 1;
+			gooddial += 1
+			var evens = _.filter(value['users'], function(str){ return str.match(/NewAgent/) != undefined });
+			if (evens != 0)	newagentgood += 1	
  			
+			var evens = _.filter(value['users'], function(str){ return str.match(/KBAgent/g) != undefined });
+			if (evens != 0)	kbagentgood += 1		
+
  		}
 }, this)
 
 console.log(data.length)
 
 console.log("gooddial "+gooddial)
+console.log("newagentgood" + newagentgood)
+console.log("kbagentgood" + kbagentgood)
 // 61 dialogues that marked as "goodconv"
 
 
