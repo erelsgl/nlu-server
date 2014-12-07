6f12d78cb0d49a0b6655f64f27e4ab23b0ac1a90
vasily <elgesto@gmail.com>
2014-12-07 12:02:09 +0200

    Add more stats functions

--- research/ppdb/stats_dial.js
+++ research/ppdb/stats_dial.js
@@ -2,6 +2,7 @@
 
 var _ = require('underscore')._; 
 var fs = require('fs');
+var utils = require('./utils') 
 
 var data = []
 
@@ -12,6 +13,9 @@ var gooddial = 0
 var newagentgood = 0
 var kbagentgood = 0
 var numberofturns = []
+var numberofintents = []
+var agentstr = ""
+var humansentences = []
 
 _.each(data, function(value, key, list){
 	if (value['status'].indexOf("goodconv") != -1)
@@ -19,15 +23,37 @@ _.each(data, function(value, key, list){
 
 			gooddial += 1
 			var evens = _.filter(value['users'], function(str){ return str.match(/NewAgent/) != undefined });
-			if (evens != 0)	newagentgood += 1	
- 			
+			if (evens != 0)	
+				{
+					newagentgood += 1	
+ 					agentstr = 'NewAgent'
+ 				}
+
 			var evens = _.filter(value['users'], function(str){ return str.match(/KBAgent/g) != undefined });
-			if (evens != 0)	kbagentgood += 1		
+			if (evens != 0)	
+				{
+					kbagentgood += 1		
+ 					agentstr = 'KBAgent'
+				}
 
 			var dial = []
-			dial.push(_.filter(value['turns'], function(turn){ return turn['status'] == 'active' }).length)
-			dial.push(_.filter(value['turns'], function(turn){ return turn['status'] == 'inactive' }).length)
+			dial.push(_.filter(value['turns'], function(turn){ return ((turn['status'] == 'active')   && (turn['user'].match(/Agent/g) != undefined)) }).length)
+			dial.push(_.filter(value['turns'], function(turn){ return ((turn['status'] == 'inactive') && (turn['user'].match(/Agent/g) != undefined)) }).length)
+			dial.push(_.filter(value['turns'], function(turn){ return ((turn['status'] == 'active')   && (turn['user'].match(/Agent/g) == undefined)) }).length)
+			dial.push(_.filter(value['turns'], function(turn){ return ((turn['status'] == 'inactive') && (turn['user'].match(/Agent/g) == undefined)) }).length)
  			numberofturns.push(dial)
+
+ 			var intents = []
+			intents.push(_.filter(value['turns'], function(turn){ return ((utils.onlyIntents(turn['output']).length == 1) && (turn['user'].match(/Agent/g) != undefined)) }).length)
+			intents.push(_.filter(value['turns'], function(turn){ return ((utils.onlyIntents(turn['output']).length > 1)  && (turn['user'].match(/Agent/g) != undefined)) }).length)
+			intents.push(_.filter(value['turns'], function(turn){ return ((utils.onlyIntents(turn['output']).length == 1) && (turn['user'].match(/Agent/g) == undefined)) }).length)
+			intents.push(_.filter(value['turns'], function(turn){ return ((utils.onlyIntents(turn['output']).length > 1)  && (turn['user'].match(/Agent/g) == undefined)) }).length)
+			intents.push(agentstr)
+			intents.push(value['id'])
+			numberofintents.push(intents)
+
+		
+ 		
  		}
 }, this)
 
@@ -35,14 +61,18 @@ console.log(data.length)
 
 console.log("gooddial "+gooddial)
 // 61 dialogues that marked as "goodconv"
-console.log("newagentgood" + newagentgood)
+console.log("newagentgood " + newagentgood)
 // 31 dialogues
-console.log("kbagentgood" + kbagentgood)
+console.log("kbagentgood " + kbagentgood)
 // 30 dialogues
 console.log(numberofturns)
 // huge variance
-console.log()
+console.log(_.reduce(numberofturns, function(memo, num){ return memo + num[0] + num[1]; }, 0) / numberofturns.length)
 // average number of turns
+console.log(_.reduce(numberofturns, function(memo, num){ return memo + num[2]; }, 0))
+// total number of  active turns
+console.log(numberofintents)
+// one/several intents
 
 
 
