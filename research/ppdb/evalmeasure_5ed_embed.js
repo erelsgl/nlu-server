ee03dc52a8f61d0fcf67c71a11e5073e835ed147
vasily <elgesto@gmail.com>
2014-12-23 14:43:31 +0200

    Improve paths

--- research/ppdb/evalmeasure_5ed_embed.js
+++ research/ppdb/evalmeasure_5ed_embed.js
@@ -16,19 +16,25 @@ var PrecisionRecall = require("limdu/utils/PrecisionRecall");
 var partitions = require('limdu/utils/partitions');
 var rules = require("../rule-based/rules.js")
 var truth = require("../rule-based/truth_utils.js")
-var truth_filename =  "../../../truth_teller/sentence_to_truthteller.txt"
+var truth_filename =  __dirname+ "/../../../truth_teller/sentence_to_truthteller.txt"
 
 var limdu = require("limdu");
 var ftrs = limdu.features;
 var regexpNormalizer = ftrs.RegexpNormalizer(
-    JSON.parse(fs.readFileSync('../../knowledgeresources/BiuNormalizations.json')));
+    JSON.parse(fs.readFileSync(__dirname+'/../../knowledgeresources/BiuNormalizations.json')));
 
-function trainandtest(train, test, seeds, callback9)
+function trainandtest(train, test, seeds, mode, callback9)
 {
 
+/*
+ mode
+ 0 regular
+ 1 sequence
+*/
   var data = []
 
   _.each(test, function(utterance, key, list){ 
+    console.log(utterance)
     var sentence = utterance['input']
     test[key]['input_original'] = sentence
     
@@ -48,20 +54,29 @@ function trainandtest(train, test, seeds, callback9)
   var test_turns = test
   var stats = new PrecisionRecall()
 
-  // utils.restartredis()
-  // console.log("start eval")
-
   async.eachSeries(test_turns, function(turn, callback1){
 
     utils.retrieveIntent(turn['input_modified'], seeds, function(err, out){
 
-      var labs = _.unique(_.map(out, function(num, key){ return Object.keys(num)[0] }))      
-      var out = stats.addCasesHash(_.unique(utils.onlyIntents(turn['output'])), _.unique(labs))
-      
+      // regular mode
+      if (mode == 0)
+      {
+        var labs = _.unique(_.map(out, function(num, key){ return Object.keys(num)[0] }))      
+        var out = stats.addCasesHash(_.unique(utils.onlyIntents(turn['output'])), _.unique(labs))
+      }
+
+      // sequence mode
+      if (mode == 1)
+      {
+        var sequence = _.map(out, function(num, key){ return [Object.keys(num)[0], num[Object.keys(num)[0]]['position']] });
+        sequence = bars.uniqueArray(sequence)
+        var out = stats.addCasesHashSeq(utils.seqgold(turn), sequence,1)
+      }
+
       callback1()
     })
   }, function(err){
-      // console.log("end eval")
+  
       var output = {}
       output['stats'] = stats.retrieveStats()
       callback9(err,output)
