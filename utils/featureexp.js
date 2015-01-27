ac0613a5d3765305f751630708859d0f883ef74b
vasily <elgesto@gmail.com>
2015-01-27 07:22:42 +0200

    Improve expansion

--- utils/featureexp.js
+++ utils/featureexp.js
@@ -1,14 +1,29 @@
 var fs = require('fs');
 var async = require('async');
+var bars = require(__dirname+"/bars.js");
 var ppdb = require(__dirname+"/../research/ppdb/utils.js");
 var _ = require('underscore')._;
 
 if (process.argv[1] === __filename)
 {
 	var output = {}
+
+	console.log("inside scale "+process.argv[2]+" phrase "+process.argv[3])
+
 	var scale = JSON.parse(process.argv[2])
+	var phrase = JSON.parse(process.argv[3])
+
+	// console.log("inside scale parse "+scale+" phrase "+phrase)
+	// console.log(scale)
+
+	// phrase:
+	// 0 - everything
+	// 1 - only unigram seed generated unigram paraphrase
+
 	var seeds = JSON.parse(fs.readFileSync(__dirname+"/featureexp_input"))
-   
+
+	seeds = seeds.splice(1,seeds.length-1)
+
    	async.eachSeries(seeds, function(seed, callback){
 		ppdb.recursionredis([seed], scale, true, function(err, results) {
 
@@ -23,10 +38,14 @@ if (process.argv[1] === __filename)
 		})
 	},function(err){
 		
+		if (phrase == 1)
+			output = bars.onlyunigrams(output)
+
 		_.each(output, function(value, key, list){ 
 			output[key] = _.sortBy(output[key], function(num){ return num[2]; });
 		}, this)
 
+		console.log("output "+Object.keys(output).length)
 		fs.writeFileSync(__dirname+"/featureexp_output", JSON.stringify(output, null, 4), 'utf-8')
 		ppdb.closeredis()
 	})
