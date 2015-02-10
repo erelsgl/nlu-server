8d8e9fb968e701f84ab8b18e4b28bc1cb4ac94c4
vasily <elgesto@gmail.com>
2015-02-10 19:19:42 +0200

    Wordnet

--- research/ppdb/wordnet.js
+++ research/ppdb/wordnet.js
@@ -1,13 +1,15 @@
 var natural = require('natural');
-
+var _ = require('underscore')._;
 var wordnet = new natural.WordNet();
 
 var sym = []
 wordnet.lookupSynonyms('offer', function(results) {
     results.forEach(function(result) {
+	// sym.push(result.synonyms)
     	// sym.push(result.lemma)
-      console.log(result)
-      console.log(result.ptrs)
+      sym = sym.concat(result.synonyms)
+      // console.log(result)
+      // console.log(result.ptrs)
         // console.log('------------------------------------');
 //        console.log(result.synsetOffset);
   ///      console.log(result.pos);
@@ -17,7 +19,7 @@ wordnet.lookupSynonyms('offer', function(results) {
 	// console.log(result)
     });
 
-    console.log(sym)
+    console.log(_.unique(sym))
     console.log()
     process.exit(0)
 });
