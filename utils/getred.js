a5d7effafbbe1c9276020741b7c261d5562a67a5
vasily <elgesto@gmail.com>
2015-03-11 14:15:51 +0200

    Improve word2vec

--- utils/getred.js
+++ utils/getred.js
@@ -1,15 +1,27 @@
 var redis = require("redis")
+var async = require('async');
+var _ = require('underscore')._;
 var client = redis.createClient(6369)
+
+var params = process.argv.slice();
+params.splice(0,2)
 
 if (process.argv[1] === __filename)
 {	
-	var word = process.argv[2]
-	client.select(15, function(err, response) {
-		client.get(word, function (err, response) {
-			console.log(response)
-			process.exit(0)
-		})
-	})
+	async.mapSeries(params, function(word, callback){
+		client.select(15, function(err, response) {
+			client.get(word, function (err, response) {
+
+				if (response == null)
+					callback(err, [])
+
+				var vec = response.split(",")
+				vec = _.map(vec, function(value){ return parseFloat(value); });
+				callback(err, vec)
+			})
+		})	
+	}, function(err, result) {
+		console.log(result)
+		process.exit(0)	
+	})	
 }
-
-
