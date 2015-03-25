c294f7453f39151a68a2323883d8f0cbe997023b
vasily <elgesto@gmail.com>
2015-03-25 13:56:55 +0200

    Add new modes tests

--- research/ppdb/mochamodes.js
+++ research/ppdb/mochamodes.js
@@ -13,10 +13,27 @@ var modes = require('./modes.js')
 describe('Util test', function() {
 
 	it('intersection', function() {	
+
 		var train = 'I go to school'
 		var test = 'usually I go to school by foot'
 		modes.strict_keyphrase(test, train).should.be.true
+
+		var train = 'I will go to school'
+		var test = 'usually I go to school by foot'
+		modes.strict_keyphrase(test, train).should.be.false
+
+		var train = 'I will go to school'
+		var test = 'I might go to school by foot'
+		modes.strict_keyphrase(test, train).should.be.true
 	})
+
+	it('predicate', function() {	
+		var train = 'I go to school'
+		var test = 'usually I go to school by foot'
+		modes.predicate(test, train, train)
+	})
+
+
 
 
 })
