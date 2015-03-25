ae353b9c0994bdb75c270c7f30a30bad4986f2c2
vasily <elgesto@gmail.com>
2015-03-25 10:24:58 +0200

    Add modes

---
+++ research/ppdb/mochamodes.js
@@ -0,0 +1,23 @@
+/**
+ * a unit-test for PrecisionRecall unit
+ * 
+ * @author Erel Segal-Halevi
+ * @since 2013-08
+ */
+
+var should = require('should');
+var utils = require('./utils');
+var _ = require('underscore');
+var modes = require('./modes.js')
+
+describe('Util test', function() {
+
+	it('intersection', function() {	
+		var train = 'I go to school'
+		var test = 'usually I go to school by foot'
+		modes.strict_keyphrase(test, train).should.be.true
+	})
+
+
+})
+
