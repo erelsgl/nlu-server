0f596fde18b153cba466a95402eebcd61147f447
vasily <elgesto@gmail.com>
2015-03-29 16:46:11 +0300

    Fix sorting

--- utils/featureexp.js
+++ utils/featureexp.js
@@ -4,9 +4,11 @@ var bars = require(__dirname+"/bars.js");
 var ppdb = require(__dirname+"/../research/ppdb/utils.js");
 var _ = require('underscore')._;
 
+var output = {}
+
 if (process.argv[1] === __filename)
 {
-	var output = {}
+	
 
 	console.log("inside scale "+process.argv[2]+" phrase "+process.argv[3])
 
@@ -21,34 +23,46 @@ if (process.argv[1] === __filename)
 	// 1 - only unigram seed generated unigram paraphrase
 
 	var seeds = JSON.parse(fs.readFileSync(__dirname+"/featureexp_input"))
+
 	console.log("featureexp read " + seeds.length + " seeds")
+	console.log("scale " + scale )
 
 	seeds = seeds.splice(1,seeds.length-1)
 
    	async.eachSeries(seeds, function(seed, callback){
 		ppdb.recursionredis([seed], scale, true, function(err, results) {
-
 			results = results.splice(1,results.length-1)
-
 			_.each(results, function(value, key, list){ 
 				if (!(value[0] in output))
 					output[value[0]] = []
 				output[value[0]].push([seed, value[1], value[2]])				
 			}, this)
-			callback()
+
+			callback(err)
 		})
 	},function(err){
-		
-		if (phrase == 1)
-			output = bars.onlyunigrams(output)
 
-		_.each(output, function(value, key, list){ 
-			output[key] = _.sortBy(output[key], function(num){ return num[2]; });
+		// if (phrase == 1)
+		// output = bars.onlyunigrams(output)
+
+		var list = _.pairs(output)
+		
+		list = _.sortBy(list, function(sublist){ return _.reduce(sublist[1], function(memo, num){ return memo + num[2]; }, 0)/sublist[1].length });
+		
+		var output1 = {}
+
+		_.each(list, function(value, key, list){ 
+			output1[value[0]] = value[1]
 		}, this)
 
-		console.log("output "+Object.keys(output).length)
+		/*_.each(output, function(value, key, list){ 
+			output[key] = _.sortBy(output[key], function(num){ return num[2]; });
+		}, this)
+		*/
+
+		console.log("output "+Object.keys(output1).length)
 		console.log("featureexp finished")
-		fs.writeFileSync(__dirname+"/featureexp_output", JSON.stringify(output, null, 4), 'utf-8')
+		fs.writeFileSync(__dirname+"/featureexp_output", JSON.stringify(output1, null, 4), 'utf-8')
 		ppdb.closeredis()
 	})
 }
