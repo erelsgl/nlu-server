52ee6e553847ee6181b87a0c706b1fcf19197bb0
vasily <elgesto@gmail.com>
2015-05-08 16:19:17 +0300

    Improve wordnet

--- utils/getwordnet.js
+++ utils/getwordnet.js
@@ -3,7 +3,10 @@
 // http://stackoverflow.com/questions/1833252/java-stanford-nlp-part-of-speech-labels
 
 // @    Hypernym 
- // ~    Hyponym 
+// ~    Hyponym 
+
+// synset diambiguation = [all, the most frequent]
+// relation = [ syn, hypo_0, cohypo ]
 
 var _ = require('underscore')._;
 var natural = require('natural');
@@ -15,42 +18,25 @@ params.splice(0,2)
 
 var POS = {
 
-	'NN': 'n',
-	'NNS': 'n',
-	'NNP': 'n',
-	'NNPS': 'n',
-
-	'JJ': 'a',
-	'JJR': 'a',
-	'JJS': 'a',
-
-	'RB': 'r',
-	'RBR': 'r',
-	'RBS': 'r',
-	'WRB': 'r',
-
-	'VB': 'v',
-	'VBD': 'v',
-	'VBG': 'v',
-	'VBN': 'v',
-	'VBP': 'v',
-	'VBZ': 'v'
+	'n': ['NN', 'NNS', 'NNP', 'NNPS'],
+	'a': ['JJ', 'JJR', 'JJS'],
+	's': ['JJ', 'JJR', 'JJS'],
+	'r': ['RB', 'RBR', 'RBS','WRB'],
+	'v': ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 }
-
 
 var string = params[0]
 var pos = params[1]
 var relation = params[2]
 var result = []
 
-if (!(pos in POS))
+if (_.flatten(_.toArray(POS)).indexOf(pos) == -1)
 {
 	console.log([])
 	process.exit(0)
 }
 
 var output = []
-// wordnet.get(4424418, 'n', function(result) {
 
 if (process.argv[1] === __filename)
 {
@@ -58,45 +44,101 @@ async.waterfall([
     function(callback) {
 
     	var output = []
-    	
+	
     	wordnet.lookup(string, function(results) {
-    		
+
     		_.each(results, function(value, key, list){
 				
-				if (value['pos'] == POS[pos])
-					output = output.concat(value) 
+				if (POS[value['pos']].indexOf(pos) != -1)
+					output.push(value) 
 
 			}, this)
 
-    		callback(null, _.unique(output))
+    		callback(null, output)
 		})
     },
     function(results, callback) {
 
     	var output = []
 
-    	_.each(results, function(value, key, list){
+    	if (relation == "synonym")
+    	{
+			_.each(results, function(value, key, list){
+				output = output.concat(value['synonyms']) 
+			}, this)
 
-			output = output.concat(value['synonyms']) 
+			callback(null, _.unique(output))
+    	}
 
-		}, this)
-
-    	if (relation == "syn")
-			callback(null, output)
-
-		if (relation == "all")
-		{
-			async.eachSeries(results, function(result, callback1){ 
+    	if (relation == "hypernym")
+    	{
+    		async.eachSeries(results, function(result, callback1){ 
 
 				async.eachSeries(result['ptrs'], function(res, callback2){
 
-					if ((res['pointerSymbol'] == '~') || (res['pointerSymbol'] == '@'))
+					if (res['pointerSymbol'] == '@')
 					{
 						wordnet.get(res['synsetOffset'], res['pos'], function(subres) {
 							output = output.concat(subres['synonyms'])
 							callback2()
 						})
+    				}
+    				else
+    				callback2()
 
+				}, function(err){ callback1() })
+			},  function(err){ callback(null, output) })
+		}
+
+		if (relation == "hyponym")
+    	{
+    		async.eachSeries(results, function(result, callback1){ 
+
+				async.eachSeries(result['ptrs'], function(res, callback2){
+
+					if (res['pointerSymbol'] == '~')
+					{
+						wordnet.get(res['synsetOffset'], res['pos'], function(subres) {
+							output = output.concat(subres['synonyms'])
+							callback2()
+						})
+    				}
+    				else
+    				callback2()
+
+				}, function(err){ callback1() })
+			},  function(err){ callback(null, output) })
+		}
+
+    	if (relation == "cohyponym")
+    	{
+		
+			async.eachSeries(results, function(result, callback1){ 
+
+				async.eachSeries(result['ptrs'], function(res, callback2){
+
+					if (res['pointerSymbol'] == '@')
+					{
+
+						wordnet.get(res['synsetOffset'], res['pos'], function(subres) {
+
+							async.eachSeries(subres['ptrs'], function(subsubres, callback3){
+					
+								if (subsubres['pointerSymbol'] == '~')
+								{
+
+									wordnet.get(subsubres['synsetOffset'], subsubres['pos'], function(susubsubbres) {
+
+										output = output.concat(susubsubbres['synonyms'])
+										callback3()
+
+									})
+								}
+								else
+								callback3()
+
+							},  function(err){ callback2() })
+						})
 					}
 					else
 					callback2()			
@@ -108,9 +150,9 @@ async.waterfall([
 				callback(null, output)	
 			
 			})
-	
-		}
-    }
+
+    	}
+	}
 ], function (err, result) {
 
 	var result = _.map(_.unique(result), function(value){ return value.replace(/\_/g," "); });
