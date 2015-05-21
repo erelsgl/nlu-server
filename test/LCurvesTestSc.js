34faa4935d4dc7df9530b3006287b71cc94575f8
vasily <elgesto@gmail.com>
2015-05-21 23:19:29 +0300

    Fix test

--- test/LCurvesTestSc.js
+++ test/LCurvesTestSc.js
@@ -15,8 +15,8 @@ describe('Learning curves utilities', function() {
 	it('filter', function() {
 	
 		var dataset = [
-		{"input":{"CORENLP":{"sentences":[1,2,3,4,5,6]}}},
-		{"input":{"CORENLP":{"sentences":[7,8,9,0]}}},
+			{"input":{"CORENLP":{"sentences":[1,2,3,4,5,6]}}},
+			{"input":{"CORENLP":{"sentences":[7,8,9,0]}}},
 		]
 
 		var data = curves.filtrain(dataset, 3)
@@ -24,5 +24,22 @@ describe('Learning curves utilities', function() {
 		_.isEqual(data[1]["input"]["CORENLP"]["sentences"], [7,8,9]).should.be.true
 
 	})
+
+	it('filter', function() {
+		var dataset = [
+			{"input":{"CORENLP":{"sentences":[1,2,3,4,5,6]}}, "output": [1]},
+			{"input":{"CORENLP":{"sentences":[7,8,9,0]}}, "output":[2]},
+			{"input":{"CORENLP":{"sentences":[7]}}, "output":[3]},
+			{"input":{"CORENLP":{"sentences":[7]}}, "output":[2]},
+			{"input":{"CORENLP":{"sentences":[7,6,7,8,9]}}, "output":[2]},
+			{"input":{"CORENLP":{"sentences":[1,2]}}, "output":[2]},
+
+		]
+		
+		var data = curves.groupbylabel(dataset, 2, 3)
+		data[2].length.should.equal(3)
+
+	})
+
 
 })
