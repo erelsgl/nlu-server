422e228f6d580f8ada410fd818c75cad54750481
vasily <elgesto@gmail.com>
2015-05-24 22:31:56 +0300

    Fix

--- utils/learning_curves_sc.js
+++ utils/learning_curves_sc.js
@@ -388,7 +388,7 @@ if (process.argv[1] === __filename)
 
 	var classifiers  = {
 				// first should be the baseline
-				// TC: cslassifier.TC,
+				TC: cslassifier.TC,
 				TCPPDB: classifier.TCPPDB
 
 			}
