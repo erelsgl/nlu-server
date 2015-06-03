6394be51c245a69e11362bc8acee2280206268eb
vasily <elgesto@gmail.com>
2015-06-03 12:33:11 +0300

    Fix

--- utils/wikipedia.js
+++ utils/wikipedia.js
@@ -48,13 +48,14 @@ function load_wikipedia(folder)
 
      		console.log("json parsing 1")
      		var tempar1 = JSON.parse(str1)
-     		console.log("json parsing 1")
+     		console.log("json parsing 2")
      		var tempar2 = JSON.parse(str2)
      		
      		console.log("concat 1")
      		ar1 = ar1.concat(tempar1)
      		console.log("concat 2")
      		ar2 = ar2.concat(tempar2)
+     		console.log("stop all")
 
      		str1 = "["
     		str2 = "["
