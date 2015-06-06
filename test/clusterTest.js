3dbeff65bf83f0fc77afc4a391b07e5dfef59954
vasily <elgesto@gmail.com>
2015-06-06 11:20:10 +0300

    Add tests

--- test/clusterTest.js
+++ test/clusterTest.js
@@ -8,7 +8,24 @@ var should = require('should');
 var master = require('../utils/master');
 var _ = require('underscore');
 
-
+var stat = {
+        "15": {
+            "0": {
+                "TC": { "0": 500, "1": 401 },
+                "TCBOC": { "0": 410, "1": 611 }
+                },
+            "1": {
+                "TC": { "0": 00, "1": 01 },
+                "TCBOC": { "0": 10, "1": 11 }
+                }
+        },
+        "30": {
+            "1": {
+                "TC": { "0": 20, "1": 21 },
+                "TCBOC": { "0": 30, "1": 31 }
+            }
+        }
+    }
 
 describe('wikipedia', function() {
 
@@ -34,8 +51,22 @@ describe('wikipedia', function() {
 
                 var trainop = master.trainlen(train, 2)
                 trainop.length.should.equal(3)
-
-	
         })
 
+        it('plotlcagrfold', function() {
+                var output = master.plotlcagr(0/*fold*/, stat)
+                _.isEqual(output, [     ["size","TC","TCBOC"],
+                                                ["15",0,10],
+                                                ["30",20,30]]).should.be.true
+
+                var str = master.getstringlc(output)
+                str.should.be.equal("size\tTC\tTCBOC\n15\t0\t10\n30\t20\t30")
+        })
+
+        it('plotlcagraverage', function() {
+                var output = master.plotlcagr('average'/*fold*/, stat)
+                _.isEqual(output, [     ["size","TC","TCBOC"],
+                                                        [ '15', 0.5, 10.5 ],
+                                                        [ '30', 20.5, 30.5 ] ]).should.be.true
+        })
 })
