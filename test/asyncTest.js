c98fa193848caf152b20c8e02fe771cb9f53133c
vasily <elgesto@gmail.com>
2015-06-07 13:09:52 +0300

    Tests

--- test/asyncTest.js
+++ test/asyncTest.js
@@ -9,6 +9,7 @@ var async_adapter = require('../utils/async_adapter');
 var _ = require('underscore');
 
 describe('async adapters', function() {
+    this.timeout(15000);
 
 	it('wordnet synonyms', function(callback) {
         async_adapter.getwordnet("offer", "VB", "synonym", function(err, results){
@@ -22,6 +23,22 @@ describe('async adapters', function() {
             results.length.should.equal(19)
             callback()
         })  
+    })
+
+    it('wordnet cache', function(callback) {
+        console.time("first")
+        async_adapter.getwordnetCache("offer", "VB", "hypernym", function(err, results){
+            console.timeEnd("first")
+            console.time("second")
+            async_adapter.getwordnetCache("run", "VB", "hypernym", function(err, results){
+                console.timeEnd("second")
+                console.time("cache")
+                async_adapter.getwordnetCache("offer", "VB", "hypernym", function(err, results){
+                    console.timeEnd("cache")
+                    callback()
+                })
+            })
+        })
     })
 
 })
