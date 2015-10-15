d8ea750d2cd54331a8780d1e709f7fe780924951
vasily <elgesto@gmail.com>
2015-10-15 11:11:31 +0300

    Improving workers

--- lc/worker.js
+++ lc/worker.js
@@ -4,109 +4,93 @@ var _ = require('underscore')._;
 var fs = require('fs');
 var wikipedia = require(__dirname + '/../utils/wikipedia');
 var master = require('./master');
-var partitions = require('limdu/utils/partitions');
 var classifiers = require(__dirname+"/../classifiers.js")
 var trainAndTest_async = require(__dirname+'/../utils/trainAndTest').trainAndTest_async;
 var clc = require('cli-color')
+var bars = require(__dirname+'/../utils/bars');
 
 var fold = process.env["fold"]
-var datafilepath = process.env["datafile"]
 var folds = process.env["folds"]
 var classifier = process.env["classifier"]
-var maxlen = process.env["len"]
 var thread = process.env["thread"]
 var msg = clc.xterm(thread)
 
 if (cluster.isWorker)
 	console.log(msg("worker: started"))
 
-var files = fs.readdirSync(datafilepath)
+process.on('message', function(message) {
+    console.log(msg('worker ' + process.pid + ' received message from master.'))
+	
+	var train = JSON.parse(message['train'])
+	var test  = JSON.parse(message['test'])
 
-var dataset_global = {}
+	console.log(msg("train is array:"+_.isArray(train) + " and its size "+train.length))
+	console.log(msg("test is array:"+_.isArray(test) + " and its size "+test.length))
 
-_.each(files, function(file, key, list){
-	console.log(msg("worker "+process["pid"]+":loading "+file))
-	dataset_global[file]= JSON.parse(fs.readFileSync(datafilepath+file))
-}, this)
+	var index = 0
 
-console.log(msg("worker "+process["pid"]+": dataset loaded"))
+	async.whilst(
+	    function () { return index <= train.length },
+	    function (callbackwhilst) {
 
-var classes = Object.keys(dataset_global)
-var datasetsize = _.flatten(_.toArray(dataset_global)).length
+			// var len = 5
+	       	index += (index < 20 ? 1 : 25)
 
-var dataset = partitions.partitions_hash_fold(dataset_global, folds, fold)
-console.log(msg("worker "+process["pid"]+": dataset partitioned"))
+	       	var mytrain = train.slice(0, index)
 
-var train = dataset['train']
-var test = dataset['test']
+	       	var mytrainex = (bars.isDialogue(mytrain) ? bars.extractdataset(mytrain) : mytrain)
+			var mytestex  = (bars.isDialogue(test) ? bars.extractdataset(test) : test)
 
-var index = 0
+	       	// var mytrainset = master.trainlen(train, index)
 
-async.whilst(
-    function () { return index <= train.length },
-    function (callbackwhilst) {
+			console.log(msg("worker "+process["pid"]+": index=" + index +
+				" train_dialogue="+mytrain.length+" train_turns="+mytrainex.length+
+				" test_dialogue="+test.length +" test_turns="+mytestex.length+
+				" classifier="+classifier+ " fold="+fold))
 
-		var len = 1
-       	index += (index < 20 ? 1 : 25)
+		    trainAndTest_async(classifiers[classifier], mytrainex, mytestex, function(err, stats){
 
-       	var mytrainset = master.trainlen(train, index)
+		    	var uniqueid = new Date().getTime()
 
-       	async.whilst(
+		    	console.log(msg("worker "+process["pid"]+": traintime="+
+		    		stats['traintime']/1000 + " testtime="+ 
+		    		stats['testtime']/1000 + " classifier="+classifier + 
+		    		" Accuracy="+stats['stats']['Accuracy']+ " fold="+fold + " id="+stats['id']))
 
-    		function () { return len <= maxlen },
-    		function (callbacktime) {
-			
-				console.log(msg("worker "+process["pid"]+": datasetsize="+datasetsize+" index=" + index +" train="+train.length+" alltrain="+_.flatten(train).length+" traincurrent="+mytrainset.length/classes.length+" testall="+test.length+" test="+test.length/classes.length +
-					" length="+len+" maxlen="+maxlen+" classifier="+classifier+" classes="+classes.length + " fold="+fold))
+		    	var stats1 = {}
+		    	_.each(stats['stats'], function(value, key, list){ 
+		    		if ((key.indexOf("F1") != -1) || (key.indexOf("Accuracy") != -1 ))
+		    			stats1[key] = value
+		    	}, this)
 
-				var mytrain = master.filtrain(mytrainset, len, 0)
-				var mytest = master.filtrain(test, len, 0)
-				
-			    trainAndTest_async(classifiers[classifier], mytrain, mytest, function(err, stats){
+				var results = {
+					'classifier': classifier,
+					'fold': fold,
+					'trainsize': mytrainset.length/classes.length,
+					'trainlen': len,
+					// 'F1': stats['stats']['F1'],
+					// 'macroF1': stats['stats']['macroF1'],
+					// 'Accuracy': stats['stats']['Accuracy'],
+					// 'stats': stats['stats'],
+					'stats': stats1,
+					'uniqueid': stats['id']
+				}
 
-			    	var uniqueid = new Date().getTime()
-
-			    	console.log(msg("worker "+process["pid"]+": traintime="+
-			    		stats['traintime']/1000 + " testtime="+ 
-			    		stats['testtime']/1000 + " classifier="+classifier + 
-			    		" Accuracy="+stats['stats']['Accuracy']+ " fold="+fold + 
-			    		" trainsize="+mytrainset.length/classes.length+ " trainlen="+
-			       	    len+ " id="+stats['stats']['id']))
-
-					var results = {
-						'classifier': classifier,
-						'fold': fold,
-						'trainsize': mytrainset.length/classes.length,
-						'trainlen': len,
-						// 'F1': stats['stats']['F1'],
-						// 'macroF1': stats['stats']['macroF1'],
-						'Accuracy': stats['stats']['Accuracy'],
-						'uniqueid': stats['stats']['id']
-					}
-
-					len += 1
-
-					process.send(JSON.stringify(results))
-					callbacktime()
-
-		    	})
-	    },
+				process.send(JSON.stringify(results))
+		   		callbackwhilst()
+		   	})
+						
+    	},
     	function (err) {
-    		callbackwhilst()
-    	}
-		)
-			
-    },
-    function (err) {
-		console.log(msg("worker "+process["pid"]+": exiting"))
-		process.exit()
-	})
+			console.log(msg("worker "+process["pid"]+": exiting"))
+			process.exit()
+		})
 			  	
-// fs.appendFileSync(statusfile, JSON.stringify(stat, null, 4))
-// console.log(JSON.parse(cluster.worker.process.argv[3]))
-// // console.log(cluster.worker.process.config)
-// setTimeout(function() {
-// process.send({ msg: 'test' })      
-//     }, _.random(10)*1000);
+	// fs.appendFileSync(statusfile, JSON.stringify(stat, null, 4))
+	// console.log(JSON.parse(cluster.worker.process.argv[3]))
+	// // console.log(cluster.worker.process.config)
+	// setTimeout(function() {
+	// process.send({ msg: 'test' })      
+	//     }, _.random(10)*1000);
 
-
+	});
