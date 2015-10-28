2625360b391806a9e348757a316a58ce35911e4d
vasily <elgesto@gmail.com>
2015-10-28 17:12:34 +0200

    Normalization

---
+++ test/normalization.js
@@ -0,0 +1,33 @@
+/*
+ * a unit-test for normalization.
+ * 
+ * @author Vasily Konovalov
+ * @since 2015-03
+ */
+var fs = require('fs');
+var should = require('should');
+var _ = require("underscore")._;
+var limdu = require("limdu");
+var ftrs = limdu.features;
+
+var regexpNormalizer = ftrs.RegexpNormalizer(
+		JSON.parse(fs.readFileSync(__dirname+'/../knowledgeresources/BiuNormalizations.json')));
+
+describe('normalization test', function() {
+
+	it('salary', function(){
+		regexpNormalizer("60K").should.equal("60000")
+		regexpNormalizer("60k").should.equal("60000")
+		regexpNormalizer("60.000").should.equal("60000")
+		regexpNormalizer("60,000").should.equal("60000")
+		regexpNormalizer(" 60 ").should.equal("60000")
+
+		regexpNormalizer("10 %").should.equal("10%")
+
+	})
+
+	
+})
+
+
+
