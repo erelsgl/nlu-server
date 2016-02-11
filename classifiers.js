45aea5722d4c6dd4eaaf9b488470b935929fed45
vasily <elgesto@gmail.com>
2016-02-11 15:12:05 +0200

    Add negation

--- classifiers.js
+++ classifiers.js
@@ -604,6 +604,51 @@ function feSplitted(sample, features, train, featureOptions, callback) {
 	callback(null, features)
 }
 
+function feNeg(sample, features, train, featureOptions, callback) {
+
+	var sentence = ""
+
+	if (_.isObject(sample)) 
+		if ("input" in sample)
+			sentence = sample.input.text
+		else
+			sentence = sample.text
+	else
+		sentence = sample
+
+	if (!('input' in sample))
+	{	
+		var sampleTemp = {}
+		sampleTemp['input']= sample
+		sample=sampleTemp
+	}
+
+	if (!('sentences' in sample['input']))
+		throw new Error("sentences not in the sample")
+
+	async.each(sample['input']['sentences'], function(sentence, callbacks) {
+
+		var root = _.find(sentence['basic-dependencies'], function(n){
+			return (n.dep == "ROOT")
+		});
+
+		var res = _.findIndex(sentence['basic-dependencies'], function(n){
+			return (n.dep=="neg" && n.governor == root.dependent)
+		});
+
+		if (res!=-1)
+			{
+				delete features[root.dependentGloss]
+				features[root.dependentGloss+"-"] = 1
+			}
+
+    	callbacks();
+  
+		}, function(err){
+			callback(null, features)
+    
+	})
+}
 
 function feContext(sample, features, train, featureOptions, callback) {
 	// offered
@@ -1078,6 +1123,7 @@ module.exports = {
 		feEmbed:feEmbed,
 		feExpansion:feExpansion,
 		feAsync:feAsync,
+		feNeg:feNeg,
 		// featureExtractorUB: featureExtractorUB,
 		// featureExtractorB: featureExtractorB,
 		// featureExtractorU: featureExtractorU,
@@ -1149,6 +1195,7 @@ module.exports = {
 		DS_comp_unigrams_async_context_offered: enhance(SvmLinearMulticlassifier, [feAsync, feContext], inputSplitter, new ftrs.FeatureLookupTable(), undefined, preProcessor_onlyIntent, postProcessor, undefined, true, {'unigrams':true, 'bigrams':false, 'allow_stopwords':true, 'offered':true, 'unoffered':false, 'previous_intent':false}),
 		
 		DS_comp_unigrams_async_context_unoffered: enhance(SvmLinearMulticlassifier, [feAsync, feContext], inputSplitter, new ftrs.FeatureLookupTable(), undefined, preProcessor_onlyIntent, postProcessor, undefined, true, {'unigrams':true, 'bigrams':false, 'allow_stopwords':true, 'offered':false, 'unoffered':true, 'previous_intent':false,'car':true}),
+		DS_comp_unigrams_async_context_unoffered_neg: enhance(SvmLinearMulticlassifier, [feAsync, feContext,feNeg], inputSplitter, new ftrs.FeatureLookupTable(), undefined, preProcessor_onlyIntent, postProcessor, undefined, true, {'unigrams':true, 'bigrams':false, 'allow_stopwords':true, 'offered':false, 'unoffered':true, 'previous_intent':false,'car':true}),
 		DS_comp_unigrams_async_context_unoffered_sim: enhance(SvmLinearMulticlassifier, [feAsync, feContext], inputSplitter, new ftrs.FeatureLookupTable(), undefined, preProcessor_onlyIntent, postProcessor, undefined, true, {'unigrams':true, 'bigrams':false, 'allow_stopwords':true, 'offered':false, 'unoffered':true, 'previous_intent':false,'car':true}),
 
 		// DS_comp_unigrams_sync_context_unoffered: enhance(SvmLinearMulticlassifier, [feAsync, feContextSync], inputSplitter, new ftrs.FeatureLookupTable(), undefined, preProcessor_onlyIntent, postProcessor, undefined, true, {'unigrams':true, 'bigrams':false, 'allow_stopwords':true, 'offered':false, 'unoffered':true, 'previous_intent':false,'car':true}),
