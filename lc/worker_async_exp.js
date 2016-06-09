b35d783246114941f2757c85fde84e6487303c70
vasily <elgesto@gmail.com>
2016-06-09 11:37:45 +0300

    Reformat

--- lc/worker_async_exp.js
+++ lc/worker_async_exp.js
@@ -44,55 +44,66 @@ process.on('message', function(message) {
 	    function () { return index < 50 },
 	    function (callbackwhilst) {
 
-		index += 5
+		async.waterfall([
+    		function(callback) {
+
+        		index += 5
 		
-	    var mytrain = train.slice(0, index)
-	    var mytrainex = JSON.parse(JSON.stringify(mytrain))
-	    var mytestex = JSON.parse(JSON.stringify(test))
+	    		var mytrain = train.slice(0, index)
+	    		var mytrainex = JSON.parse(JSON.stringify(mytrain))
+	    		var mytestex = JSON.parse(JSON.stringify(test))
 
-//	    var mytrainex = (bars.isDialogue(mytrain) ? _.flatten(mytrain) : mytrain)
-//		var mytestex  = (bars.isDialogue(test) ? _.flatten(test) : test)
+				// var mytrainex = (bars.isDialogue(mytrain) ? _.flatten(mytrain) : mytrain)
+				// var mytestex  = (bars.isDialogue(test) ? _.flatten(test) : test)
 
-//          var mytrainex =  bars.processdatasettrain(_.flatten(mytrain))
-  //        var mytestex  = bars.processdatasettest(_.flatten(test))
+				// var mytrainex =  bars.processdatasettrain(_.flatten(mytrain))
+				// var mytestex  = bars.processdatasettest(_.flatten(test))
+				console.vlog("DEBUG: worker "+process["pid"]+": index=" + index +
+					" train_dialogue="+mytrain.length+" train_turns="+mytrainex.length+
+					" test_dialogue="+test.length +" test_turns="+mytestex.length+
+					" classifier="+classifier+ " fold="+fold)
+				
+				if (classifier=="NLU_Exp")
+					bars.expanbal(mytrainex, function(err, gentra){
+						console.vlog("DEBUG: worker: "+classifier+" train size:"+gentra.length)
+						callback(null, gentra)
+					})
+				else
+					callback(null, mytrainex, mytestex)
 
-		console.vlog("DEBUG: worker "+process["pid"]+": index=" + index +
-			" train_dialogue="+mytrain.length+" train_turns="+mytrainex.length+
-			" test_dialogue="+test.length +" test_turns="+mytestex.length+
-			" classifier="+classifier+ " fold="+fold)
+    		},
+    		function(mytrainex, mytestex, callback) {
+    	
+    			trainAndTest.trainAndTest_async(classifiers[classifier], bars.copyobj(mytrainex), bars.copyobj(mytestex), function(err, stats){
 
-    	// stats = trainAndTest_hash(classifiers[classifier], mytrainex, mytestex, false)
-	
-		trainAndTest.trainAndTest_async(classifiers[classifier], bars.copyobj(mytrainex), bars.copyobj(mytestex), function(err, stats){
+				console.vlog("DEBUG: worker "+process["pid"]+": traintime="+
+					stats['traintime']/1000 + " testtime="+ 
+					stats['testtime']/1000 + " classifier="+classifier + 
+					" Accuracy="+stats['stats']['Accuracy']+ " fold="+fold)
 
-		console.vlog("DEBUG: worker "+process["pid"]+": traintime="+
-			stats['traintime']/1000 + " testtime="+ 
-			stats['testtime']/1000 + " classifier="+classifier + 
-			" Accuracy="+stats['stats']['Accuracy']+ " fold="+fold)
+				var stats1 = {}
+				_.each(stats['stats'], function(value, key, list){ 
+				   		if ((key.indexOf("Precision") != -1) || (key.indexOf("Recall") != -1 ) || (key.indexOf("F1") != -1) || (key.indexOf("Accuracy") != -1))
+				   			stats1[key] = value
+				}, this)
 
-		// console.log(JSON.stringify(stats['stats'], null, 4))
+				console.vlog("STATS: fold:"+fold+" trainsize:"+mytrain.length+" classifier:"+classifier+" "+JSON.stringify(stats1, null, 4))
 
-		var stats1 = {}
-		_.each(stats['stats'], function(value, key, list){ 
-		   		if ((key.indexOf("Precision") != -1) || (key.indexOf("Recall") != -1 ) || (key.indexOf("F1") != -1) || (key.indexOf("Accuracy") != -1))
-		   			stats1[key] = value
-		  	}, this)
+				var results = {
+					'classifier': classifier,
+					'fold': fold,
+					'trainsize': mytrain.length,
+					'trainsizeuttr': mytrainex.length,
+					'stats': stats1			
+				}
 
-		console.vlog("STATS: fold:"+fold+" trainsize:"+mytrain.length+" classifier:"+classifier+" "+JSON.stringify(stats1, null, 4))
-
-		var results = {
-			'classifier': classifier,
-			'fold': fold,
-			'trainsize': mytrain.length,
-			'trainsizeuttr': mytrainex.length,
-			'stats': stats1
-			// 'uniqueid': stats['id']
-		}
-
-		process.send(JSON.stringify(results))
-		callbackwhilst()
-	 	})
-	},
+				process.send(JSON.stringify(results))
+				callbackwhilst()
+			 	})
+		    			
+    		}], function (err, result) {
+   
+		})},
     function (err) {
 			console.log(msg("DEBUG: worker "+process["pid"]+": exiting"))
 			process.exit()
