31c4b3132a9ff5fb8c65286cdaba79dbcf778586
vasily <elgesto@gmail.com>
2016-10-26 09:34:52 +0300

    Clean up

--- lc/worker_async_initial.js
+++ lc/worker_async_initial.js
@@ -20,48 +20,32 @@ if (cluster.isWorker)
 	process.on('message', function(message) {
 	
 	var classifier = process.env["classifier"]
-	var fold = process.env["fold"]
-	
-   	console.vlog('DEBUG: worker ' + process.pid + ' received message from master.')
-	
-	//var train = bars.processdataset(_.flatten(JSON.parse(message['train'])), {"intents": true, "filter":true})
+	var fold = process.env["fold"]		
 	var train = JSON.parse(message['train'])
 	var test = JSON.parse(message['test'])
 
-/*	if (classifier == "Natural")
-		var test  = bars.processdataset(_.flatten(JSON.parse(message['test'])), {"intents": true, "filter":false})
-	else
-		var test  = _.flatten(JSON.parse(message['test']))
-*/
 	console.vlog("DEBUG: worker "+process.pid+" : train.length="+train.length + " test.length="+test.length + " classifier "+classifier)
 	var index = 1
 
 	async.whilst(
 	    function () { return index <= train.length },
-	    //function () { return index < 70 },
 	    function (callbackwhilst) {
 
-	/*if (index<10)
-			{ index += 1} 
-		else if (index<20)
-			{ index += 5 }
-		else index += 5
-*/
-// on dialogue level
 	    var mytrain = bars.copyobj(train.slice(0, index))
 	    index += 1
-	   // var mytrainex =  bars.processdataset(_.flatten(mytrain), {"intents": true, "filter":true})
+
 	    var mytestex  = []
 		var mytrainex = []
-
-	      if (classifier.indexOf("Natural")!=-1)
+		
+		if (classifier.indexOf("Natural")!=-1)
 		{
-	    		mytrainex =  bars.processdataset(_.flatten(bars.copyobj(mytrain)), {"intents": false, "filter":false, "filterIntent":[]})
-              		mytestex  = bars.processdataset(_.flatten(bars.copyobj(test)), {"intents": false, "filter":false, "filterIntent":[]})
+			mytrainex =  bars.processdataset(_.flatten(bars.copyobj(mytrain)), {"intents": false, "filter":false, "filterIntent":[]})
+			mytestex  = bars.processdataset(_.flatten(bars.copyobj(test)), {"intents": false, "filter":false, "filterIntent":[]})
 		}             
-             if (classifier.indexOf("Component")!=-1)
+        
+        if (classifier.indexOf("Component")!=-1)
 		{                
-	    	    mytrainex =  bars.processdataset(_.flatten(bars.copyobj(mytrain)), {"intents": true, "filter":false, "filterIntent":[]})
+	    	mytrainex =  bars.processdataset(_.flatten(bars.copyobj(mytrain)), {"intents": true, "filter":false, "filterIntent":[]})
 		    mytestex  = _.flatten(bars.copyobj(test))
 		}
 
@@ -157,12 +141,9 @@ if (cluster.isWorker)
 		   		callbackwhilst()
  	
 
-	})}
-	,
-        function (err) {
-                        console.vlog("DEBUG: worker "+process["pid"]+": exiting")
-                        process.exit()
-
+	})}, function (err) {
+    	console.vlog("DEBUG: worker "+process["pid"]+": exiting")
+        process.exit()
 	})
 })
 
@@ -176,9 +157,7 @@ if (cluster.isMaster)
 
 	//var classifiers = [ 'Natural','Natural_trans','Biased_no_rephrase','Biased_no_rephrase_trans']
 	//var classifiers = [ "Natural", "Natural+Context", "Component", "Component+Context" ]
-	
 	var classifiers = [ "Natural_SVM", "Natural_ADA", "Natural_RF", "Component", "Component+Context", "Natural_SVM_Context", "Natural_ADA_Context", "Natural_RF_Context" ]
-	
 
 	cluster.setupMaster({
   	exec: __filename,
@@ -186,44 +165,39 @@ if (cluster.isMaster)
 	// silent: false
 	});
 
-		var data = JSON.parse(fs.readFileSync(__dirname+"/../../negochat_private/parsed_finalized_fin_full_biased.json"))
-		console.mlog("number of unprocessed dialogues: "+data.length)
-
-		var utterset = bars.getsetcontext(data)
-		var dataset = utterset["train"].concat(utterset["test"])
+	var data = JSON.parse(fs.readFileSync(__dirname+"/../../negochat_private/parsed_finalized_fin_full_biased.json"))
+	console.mlog("number of unprocessed dialogues: "+data.length)
+	var utterset = bars.getsetcontext(data)
+	var dataset = utterset["train"].concat(utterset["test"])
 			
-		_(folds).times(function(fold){
+	_(folds).times(function(fold){
 			
-			var data = partitions.partitions_consistent_by_fold(bars.copyobj(dataset), folds, fold)
-			_.each(classifiers, function(classifier, key, list){ 
+		var data = partitions.partitions_consistent_by_fold(bars.copyobj(dataset), folds, fold)
+		
+		_.each(classifiers, function(classifier, key, list){ 
 			
-				var worker = cluster.fork({ 'fold': fold, 'classifier':classifier })
-				console.mlog("start worker")
-				
-			//	var data = partitions.partitions_consistent_by_fold(bars.copyobj(dataset), folds, fold)
-				console.mlog("DEBUGMASTER: classifier: "+classifier+" fold: "+ fold + " train size "+data.train.length + " test size " + data.test.length+" process: "+worker.process.id)
+			var worker = cluster.fork({ 'fold': fold, 'classifier':classifier })
+			console.mlog("start worker")
+			console.mlog("DEBUGMASTER: classifier: "+classifier+" fold: "+ fold + " train size "+data.train.length + " test size " + data.test.length+" process: "+worker.process.id)
 
-				worker.send({ 		
-						'train': JSON.stringify(data.test), 
-						'test': JSON.stringify(data.train)
-						})
+			worker.send({'train': JSON.stringify(data.test), 'test': JSON.stringify(data.train)})
 
-				worker.on('disconnect', function(){
-  					console.mlog("DEBUGMASTER: finished: number of clusters: " + Object.keys(cluster.workers).length)
-  					console.mlog("DEBUGMASTER: "+_.keys(statt))
-					_.each(statt, function(data, param, list){ 
-  						console.mlog("plotlc")
-						lc.plotlc('average', param, statt, lcfolder) 
-					})
-				})
-
-				worker.on('message', function(message){
-					var workerstats = JSON.parse(message)
-					workerstats['classifiers'] = classifiers
-					//console.mlog("DEBUGMASTER: on message: "+message)
-					lc.extractGlobal(workerstats, statt)
+			worker.on('disconnect', function(){
+  				console.mlog("DEBUGMASTER: finished: number of clusters: " + Object.keys(cluster.workers).length)
+  				console.mlog("DEBUGMASTER: "+_.keys(statt))
+				_.each(statt, function(data, param, list){ 
+  					console.mlog("plotlc")
+					lc.plotlc('average', param, statt, lcfolder) 
 				})
 			})
-		}, this)
+
+			worker.on('message', function(message){
+				var workerstats = JSON.parse(message)
+				workerstats['classifiers'] = classifiers
+				//console.mlog("DEBUGMASTER: on message: "+message)
+				lc.extractGlobal(workerstats, statt)
+			})
+		})
+	}, this)
 		
 }
